5fef071dc68e70f34629f4026346e92b681f3f05
Yukichi Kawada <yukichikawada@gmail.com>
2024-07-17 12:56:01 -0400

    Initial Commit

---
+++ WhopInterview/WhopInterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhopInterviewApp.swift
+//  WhopInterview
+//
+//  Created by Yukichi Kawada on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct WhopInterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
